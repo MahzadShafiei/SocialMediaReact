171612ef363791ae4c6dd00f937c28908c159d0b
Mahzad.Shafiei <Mahzad.Shafiei@IT-KHORSAND>
2023-05-24 16:59:16 +0330

    New Activity Model

---
+++ Domain/Activity.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Domain
+{
+    internal class Activity
+    {
+        public Guid Id { get; set; }
+        public string? Title { get; set; }
+        public DateTime Date { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
+        public string? City { get; set; }
+        public string? Venue { get; set; }
+    }
+}
