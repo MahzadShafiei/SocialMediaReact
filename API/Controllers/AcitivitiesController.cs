fc0cdf75703c5b28ea537e38804bfaed40cc5a71
Mahzad.Shafiei <Mahzad.Shafiei@IT-KHORSAND>
2023-05-31 17:13:07 +0330

    #3

---
+++ API/Controllers/AcitivitiesController.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers;
+
+public class AcitivitiesController : BaseApiController
+{
+    private readonly DataContext context;
+
+    public AcitivitiesController(DataContext context) => this.context = context;
+
+    [HttpGet] //api/activities
+    public async Task<ActionResult<List<Activity>>> GetActivities() => await context.Activities.ToListAsync();
+
+    [HttpGet("{id}")] //api/activities/klhjnikhni
+    public async Task<ActionResult<Activity?>> GetActivity(Guid id) => await context.Activities.FindAsync(id); 
+}
