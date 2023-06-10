ff2b179a0f2223db9cf8600cb6e46bbbb1f74f82
Mahzad.Shafiei <Mahzad.Shafiei@IT-KHORSAND>
2023-06-10 14:42:13 +0330

    #6

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Header , List} from 'semantic-ui-react';
 
 function App() {
 
@@ -16,15 +17,14 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-       <ul>
+      <Header as='h2' icon='users' content='Reactivities' />
+       <List>
         {activities.map((activity:any)=>
-        <li key={activity.id}>
+        <List.Item key={activity.id}>
         {activity.title}
-        </li>
+        </List.Item>
         )}
-       </ul>
-      </header>
+       </List>     
     </div>
   );
 }
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,10 +8,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render(  
+    <App />  
 );
 
 // If you want to start measuring performance in your app, pass a function
