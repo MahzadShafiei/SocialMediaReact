1ae0e64873916392319b01f203168ba723f69161
Mahzad.Shafiei <Mahzad.Shafiei@IT-KHORSAND>
2023-06-20 17:17:26 +0330

    #12

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header , List} from 'semantic-ui-react';
+import { Container, Header , List} from 'semantic-ui-react';
 import { Activity } from '../models/activity';
+import NavBar from './NavBar';
 
 function App() {
 
@@ -15,16 +16,18 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      <Header as='h2' icon='users' content='Reactivities' />
-       <List>
+    <Fragment>
+      <NavBar/>
+      <Container style={{marginTop:'7em'}}>
+      <List>
         {activities.map((activity)=>
         <List.Item key={activity.id}>
         {activity.title}
         </List.Item>
         )}
-       </List>     
-    </div>
+       </List> 
+      </Container>           
+    </Fragment>
   );
 }
 
