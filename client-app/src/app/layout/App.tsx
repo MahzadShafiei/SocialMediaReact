c1e883992d6d20a62c0191a779bec69f87047360
Mahzad.Shafiei <Mahzad.Shafiei@IT-KHORSAND>
2023-06-20 18:06:14 +0330

    #13

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { Container, Header , List} from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 function App() {
 
@@ -19,13 +20,7 @@ function App() {
     <Fragment>
       <NavBar/>
       <Container style={{marginTop:'7em'}}>
-      <List>
-        {activities.map((activity)=>
-        <List.Item key={activity.id}>
-        {activity.title}
-        </List.Item>
-        )}
-       </List> 
+      <ActivityDashboard activities={activities}/>
       </Container>           
     </Fragment>
   );
